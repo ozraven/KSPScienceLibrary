0854c951b189b8cf003f3cf5fc8c3adb959381d9
SpiritOffice <spiritoffice@gmx.de>
2014-07-07 12:20:36 +0200

    added TextReplacer. This can be later used to change some experiment names, or for translating the whole mod.

---
+++ TextReplacer.cs
@@ -0,0 +1,13 @@
+﻿public static class TextReplacer
+{
+    private static ConfigNode cn;
+    private static string filename = "GameData/KSPScienceLibrary/TextReplacer.cfg";
+    private static bool readOnce = false;
+
+    public static string GetReplaceForString(string input)
+    {
+        if (cn == null || !readOnce)
+            cn = ConfigNode.Load(filename) ?? new ConfigNode();
+        return cn.HasValue(input) ? cn.GetValue(input) : input;
+    }
+}
